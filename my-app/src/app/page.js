ef53ad9ad67fa298efb04211f87d89b8f42a5ac4
Stovakx <112606493+Stovakx@users.noreply.github.com>
2023-11-29 00:22:11 +0100

    key for carCards

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -36,7 +36,7 @@ export default async function Home({ searchParams }) {
           <section>
           <div className="home__cars-wrapper">
             {allCars?.map((car) => (
-              <CarCard car={car} />
+              <CarCard car={car} key={car.id} />
             ))}
           </div>
             <ShowMore
